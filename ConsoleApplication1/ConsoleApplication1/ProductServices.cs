93d8257a6860659ba497558d60c1bfa1c6f86cec
Alex <aleqsd@gmail.com>
2017-01-15 10:08:25 +0100

    TP terminé

---
+++ ConsoleApplication1/ConsoleApplication1/ProductServices.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// Developed by :
+// Alexandre DO-O ALMEIDA
+// Louis BEAUMONT
+// 16/01/2017
+
+namespace ConsoleApplication1
+{
+    class ProductServices
+    {
+        private ProductDataContext dataContext;
+        private IEnumerable<Products> productQuery;
+
+        // Constructeur ProductServices
+        public ProductServices() {
+            dataContext = new ProductDataContext();
+        }
+
+        // Retourne l’ensemble des produits
+        public IEnumerable<Products> GetAll() {
+            return productQuery = from product in dataContext.Products select product;
+        }
+
+        // Retourne le produit dont l’identifiant est passé en paramètre
+        public IEnumerable<Products> GetProductById(int id) {
+            return productQuery = from product in dataContext.Products where product.ProductID == id select product;
+        }
+
+        // Retourne tous les produits du fournisseur dont l’identifiant est passé en paramètre (ProviderId)
+        public IEnumerable<Products> GetProductsByProvider(int id) {
+            return productQuery = from product in dataContext.Products where product.SupplierID == id select product;
+        }
+
+        //Retourne tous les produits du fournisseur ayant l’identifiant idProvider et relevant de la catégorie d’identifiant idCategory
+        public IEnumerable<Products> GetProductsByProviderAndCategory(int idProvider, int idCategory) {
+            return productQuery = from product in dataContext.Products where product.SupplierID == idProvider && product.CategoryID == idCategory select product;
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// Developed by :
+// Alexandre DO-O ALMEIDA
+// Louis BEAUMONT
+// 16/01/2017
 
 namespace ConsoleApplication1
 {
@@ -10,8 +13,33 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            DataClasses1DataContext dataContext = new DataClasses1DataContext();
-            Console.WriteLine(dataContext.Customers.Count());
+            ProductServices productServices = new ProductServices();
+
+            // Liste de tous les produits
+            IEnumerable<Products> productQuery = productServices.GetAll();
+
+            // Exercice 1 
+            IEnumerable<Products> exerciceUn = productQuery.OrderBy(p => p.UnitPrice).Take(3);
+
+            Console.WriteLine("ProductName des 3 produits les plus chers : \n");
+            foreach (var Products in exerciceUn) {
+                Console.WriteLine(Products.ProductName);
+            }
+
+            // Exercice 2
+            IEnumerable<Products> exerciceDeux = productQuery.OrderBy(p => p.ProductName).Where(p => p.Discontinued);
+
+            Console.WriteLine("\nProductName triés par ordre alphabétique croissant des produits en rupture de stock (discontinued) : \n");
+            foreach (var Products in exerciceDeux) {
+                Console.WriteLine(Products.ProductName);
+            }
+
+            // Exercice 3
+            Console.WriteLine("\nNombre de produits en stocks relevant du fournisseur 1 et de la catégorie 1 :\n" + productServices.GetProductsByProviderAndCategory(1, 1).Count());
+
+            // Exercice 4
+            Console.WriteLine("\nPrix moyen des articles relevant de la catégorie 2 :\n" + productQuery.Where(p => p.CategoryID == 2).Average(p => p.UnitPrice));
+
             Console.ReadLine();
         }
     }
